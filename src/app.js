29cd38eeb9953483ff8412214d0e43abf0543407
csengekulker <k71608883429@ktch.hu>
2022-10-12 09:05:19 +0200

    editing functional + refactor

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const addButton = document.querySelector("#addButton");
 const empTable = document.querySelector("#empTable");
 const empName = document.querySelector("#name");
+const editor_id = document.querySelector('#edited_id')
+const editor_name = document.querySelector('#edited_name')
+const saveButton = document.querySelector('#saveButton')
 var tbody = document.createElement('tbody');
 empTable.appendChild(tbody);
 
@@ -8,18 +11,15 @@ const host = 'http://localhost:3000';
 
 
 (()=>{
-   console.log("kívül");
    getEmployees();
-   
 })();
 
 function getEmployees() {
     let endpoint = 'employees';
-    let url = host + '/' + endpoint;
+    let url = `${host}/${endpoint}`
     fetch(url)
     .then( response => response.json())
     .then( result => {
-        console.log(result[0].name);
         renderTable(result);
     })
     .catch(error => {
@@ -53,8 +53,7 @@ function renderTable(employees) {
 
 function makeDelButton(id) {
     let delBtn = document.createElement('button');
-    delBtn.classList.add('btn');
-    delBtn.classList.add('btn-info');
+    delBtn.classList = 'btn btn-danger ms-0'
     delBtn.textContent = 'Törlés';
     delBtn.addEventListener('click', ()=> {
         let answer = confirm('Biztosan törlöd?');
@@ -74,7 +73,7 @@ addButton.addEventListener('click', () => {
 
 function addEmployee() {
     let endpoint = 'employees';
-    let url = host + '/' + endpoint;    
+    let url = `${host}/${endpoint}`   
     let employee = {
         name: empName.value
     };
@@ -98,40 +97,58 @@ function addEmployeeToTable(employee) {
     let tr = document.createElement('tr');
     let tdId = document.createElement('td');
     let tdName = document.createElement('td');
-    let tdButton = document.createElement('td');
+    let tdDel = document.createElement('td');
+    let tdEdit = document.createElement('td');
+
  
-    tdId.textContent = employee.id;
-    tdName.textContent = employee.name;
+    tdId.textContent = employee.id
+    tdName.textContent = employee.name
 
-    tr.appendChild(tdId);
-    tr.appendChild(tdName);
-    tr.appendChild(tdButton);
+    tr.appendChild(tdId)
+    tr.appendChild(tdName)
+    tr.appendChild(tdDel)
+    tr.appendChild(tdEdit)
 
-    let delButton = makeDelButton(employee.id);
-    tdButton.appendChild(delButton);
-    tbody.appendChild(tr);
+    let delButton = makeDelButton(employee.id)
+    let editButton = makeEditButton(employee)
+
+    tdDel.appendChild(delButton)
+    tdDel.appendChild(editButton)
+    tbody.appendChild(tr)
 }
 
 function deleteEmployee(id) {
-    console.log(id);
     let endpoint = 'employees';
-    let url = host + '/' + endpoint + '/' + id;
+    let url = `${host}/${endpoint}/${id}`
     fetch(url, {
         method: 'delete'
     })
     .then(response => response.json())
     .then(result => {
-        console.log(result);
-    });
+        console.log(result)
+    })
 }
 
 function makeEditButton(employee) {
-    let editBtn = document.createElement('button');
-    editBtn.classList.add('btn');
-    editBtn.classList.add('btn-info');
-    editBtn.textContent = 'Módosítás';
+    let editBtn = document.createElement('button')
+
+    editBtn.classList.add('btn')
+    editBtn.classList.add('btn-info')
+    editBtn.textContent = 'Módosítás'
+    editBtn.classList = 'btn btn-warning ms-3'
+
+    editBtn.setAttribute('data-id', employee.id)
+    editBtn.setAttribute('data-name', employee.name)
+
+
     editBtn.addEventListener('click', ()=> {
-        console.log('Szerkesztés működik');
-    });
-    return editBtn;
+        editor_id.value = editBtn.dataset.id
+        editor_name.value = editBtn.dataset.name
+    })
+
+    return editBtn
+}
+
+function editEmployee() {
+
 }
